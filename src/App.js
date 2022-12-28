8b90106889f65c37bbb1d3cc1e86005a4844bf5a
SUMERA PUNEKAR <sumerapunekar1999@gmail.com>
2022-12-28 22:57:19 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './Component/Home';
-import SignIn from './Component/SignIn'
+import Home from './Screens/Home';
+import SignIn from './Screens/SignIn'
 import { useState } from 'react';
 
 function App() {
